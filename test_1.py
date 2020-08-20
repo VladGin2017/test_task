c052e077b5dc8dc916b8e95ca0e74e354fc7ad4d
Vladislav Ginger <vladislav@MacBook-Air-Vladislav.local>
2020-08-21 01:42:58 +0700

    Searching in Yandex

--- test_1.py
+++ test_1.py
@@ -1,37 +1,38 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
 driver.get('https://yandex.ru/')
 
 
-def search_box():
+def search_box():  # проверка поле ввода
     try:
         searching_box = driver.find_element_by_id('text')
         searching_box.send_keys('Тензор')
         print("Поле ввода найдено")
 
-    except:
+    except NoSuchElementException:
         print("Поле ввода не найдено")
 
 
-def check_suggests():
+def check_suggests():  # проверка появления подсказок
     try:
         checking_suggests = driver.find_element_by_class_name('mini-suggest_open')
         print("Подсказки найдены")
 
-    except:
+    except NoSuchElementException:
         print("Подсказки не найдены")
 
 
-def enter():
+def enter():  # имитация нажатия клавиши Enter
     try:
         entering = driver.find_element_by_id('text')
         entering.send_keys(Keys.ENTER)
         print("Ввод клавишей Enter")
 
-    except:
+    except NoSuchElementException:
         print("Ввод клавишей Enter недествует")
 
 
